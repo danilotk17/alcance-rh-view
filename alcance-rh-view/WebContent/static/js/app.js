56ae7f01acd0e709fee74a33f701fa85c91415c2
Danilo Rodrigues <danilo.tk17@gmail.com>
2016-09-28 16:48:15 -0300

    Incluindo arquivos do AngularJS

--- alcance-rh-view/WebContent/static/js/app.js
+++ alcance-rh-view/WebContent/static/js/app.js
@@ -1,1 +1,13 @@
-var app = angular.module("app", []);
+var app = angular.module("app", [ 'ngRoute' ]);
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		controller : 'HomeController',
+		templateUrl : '/static/pages/home.html'
+	}).when('/empresa', {
+		controller : 'EmpresaController',
+		templateUrl : '/static/pages/empresa.html'
+	}).otherwise({
+		redirectTo : '/'
+	});
+});
