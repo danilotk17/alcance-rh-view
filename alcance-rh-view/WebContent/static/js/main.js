2ea9852183e2487e5b4d99334482a5ab7d99283c
Danilo Rodrigues <danilo.tk17@gmail.com>
2016-10-04 16:22:07 -0300

    Commit qualquer

--- alcance-rh-view/WebContent/static/js/main.js
+++ alcance-rh-view/WebContent/static/js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
 	   $(".nav").find(".active").removeClass("active");
 	   $(this).parent().addClass("active");
 	});
+	
+	$(".logo").on("click", function() {
+	   $(".nav").find(".active").removeClass("active");
+	   $("#home-tab").addClass("active");
+	});
 });
